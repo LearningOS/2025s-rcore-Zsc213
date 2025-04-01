7a101b68a7f46daebc0f2fd2ed566748d111d432
Zsc <zsc2333@outlook.com>
2025-04-01 17:08:39 +0800

    trace

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -41,5 +41,13 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => unsafe { *(_id as *const u8) as isize },
+        1 => unsafe {
+            *(_id as *const u8) = _data;
+            0
+        },
+        2 => 0,
+        _ => -1,
+    }
 }
